ca736775e0883d253eccac2930845198ccbe1cde
sz26963632 <110951519+sz26963632@users.noreply.github.com>
2022-08-10 13:23:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
-birthday2 = os.environ['BIRTHDAY2']
+birthday2 = os.environ['BIRTHDAY']
 
 
 app_id = os.environ["APP_ID"]
